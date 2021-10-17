922da3d030c6cb0696102396f3637a8f6be27413
Jason Wang <ff1986755087@gmail.com>
2021-10-18 01:04:35 +0800

    Update 2021-10-17T17:04:31.005Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -11,61 +11,47 @@ var app = Vue.createApp({
         img: ['slamp', 'slamp', 'slamp', 'slamp']
       }],
       current_model: 0,
-      navBefore: [{
+      nav: [{
         link: '#',
         text: '產品特色'
       }, {
         link: '#',
-        text: '產品規格'
+        text: '產品優勢'
       }, {
         link: '#',
-        text: '咖啡浪潮'
-      }],
-      navAfter: [{
-        link: '#',
-        text: '設計理念'
+        text: '品牌故事'
       }, {
         link: '#',
-        text: '設備效益'
+        text: '支援中心'
       }, {
         link: '#',
-        text: '合作廠商'
+        text: '聯絡我們'
       }],
       feature_product: [{
-        col: 'col-span-1',
         icon: '',
-        text: '智慧注水設計，配合手法萃取咖啡最佳風味，高度穩定的咖啡品質'
+        text: '專利螺旋注水設計'
       }, {
-        col: 'col-span-1',
         icon: '',
-        text: '螺旋注水由內而外，模擬咖啡師手法'
+        text: '咖啡品質穩定一致'
       }, {
-        col: 'col-span-1',
         icon: '',
-        text: '雙鍋爐穩定水溫，維持連續沖煮溫度'
+        text: '雙鍋爐提供穩定水溫，可連續沖煮'
       }, {
-        col: 'col-span-2',
         icon: '',
-        text: ''
+        text: '自由設定沖煮參數'
       }, {
-        col: 'col-span-1',
         icon: '',
-        text: '客製化專屬手法參數設定，(三組儲存空間)'
-      }, {
-        col: 'col-span-1',
-        icon: '',
-        text: '每杯都是專業咖啡師手法沖煮，減少人員能力需求及培訓成本'
-      }, {
-        col: 'col-span-1',
-        icon: '',
-        text: '手機平板APP直覺式操作，智慧化操作設定'
-      }, {
-        col: 1,
-        icon: '',
-        text: '滿足連續大量出杯需求，支援快速展店體系'
+        text: '一鍵沖煮不費力'
       }],
-      coffee_before: ['確保咖啡品質', '建構優質服務', '獨特品質與差異', '完整的咖啡體驗'],
-      coffee_after: ['提升咖啡品質', '強調永續發展', '較淺的烘焙程度', '創新的沖煮方法'],
+      benefits: [{
+        text: '兼顧專注手沖和細微客戶服務'
+      }, {
+        text: '減少人事訓練成本'
+      }, {
+        text: '快速展店'
+      }, {
+        text: '咖啡品質穩定'
+      }],
       design_concept: [{
         icon: '',
         text: '兼顧專注手沖和細微客戶服務'
@@ -84,41 +70,6 @@ var app = Vue.createApp({
       }, {
         icon: '',
         text: '降低快速展店門檻'
-      }],
-      benefit_shop: [{
-        icon: '',
-        text: '品質穩定'
-      }, {
-        icon: '',
-        text: '連續沖煮'
-      }, {
-        icon: '',
-        text: '一鍵沖煮減少對人的依賴'
-      }, {
-        icon: '',
-        text: '降低訓練成本'
-      }, {
-        icon: '',
-        text: '精品咖啡風味完整重現'
-      }, {
-        icon: '',
-        text: '設備良率高穩定輸出'
-      }, {
-        icon: '',
-        text: '保留與客戶互動交流的空間'
-      }],
-      benefit_user: [{
-        icon: '',
-        text: '完整接收烘豆師欲傳達的風味和理想'
-      }, {
-        icon: '',
-        text: '享受店家優質服務'
-      }, {
-        icon: '',
-        text: '完整的咖啡體驗'
-      }, {
-        icon: '',
-        text: '穩定品質咖啡'
       }],
       partner: ['Group', 'Rectangle', 'Rectangle-1', 'Rectangle-2', 'Rectangle-3', 'Rectangle-4', 'Rectangle-5', 'Rectangle-6', 'Rectangle-7']
     };
