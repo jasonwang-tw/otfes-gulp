0b95e4126e63af5cd8606e7eb39a43b9561776b8
Jason Wang <ff1986755087@gmail.com>
2021-10-29 21:41:51 +0800

    增加文字&圖片 微改版型

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -37,29 +37,34 @@ const app = Vue.createApp({
       ],
       feature_product: [
         {
-          icon: 'bi bi-droplet',
+          // icon: 'bi bi-droplet',
+          img: '1',
           title: '專利螺旋注水設計',
-          text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+          text: '取得台灣、日本、美國、中國等國家專利'
         },
         {
-          icon: 'bi bi-cup',
+          // icon: 'bi bi-cup',
+          img: '2',
           title: '咖啡品質穩定一致',
-          text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+          text: '固定水量、水溫、萃取時間等要素，確保每杯咖啡都符合預設的品質，杯杯盡是最佳風味。'
         },
         {
-          icon: 'bi bi-moisture',
+          // icon: 'bi bi-moisture',
+          img: '3',
           title: '雙鍋爐提供穩定水溫，可連續沖煮',
-          text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+          text: '優異的控溫能力，連續沖煮咖啡也不失溫，保證咖啡的預設沖煮溫度一致。'
         },
         {
-          icon: 'bi bi-clipboard-data',
+          // icon: 'bi bi-clipboard-data',
+          img: '4',
           title: '自由設定沖煮參數',
-          text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+          text: '可依咖啡豆特性設定最佳沖煮手法。'
         },
         {
-          icon: 'bi bi-hand-index-thumb',
+          // icon: 'bi bi-hand-index-thumb',
+          img: '5',
           title: '一鍵沖煮不費力',
-          text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+          text: '簡單一鍵即可進行全自動沖煮，輕鬆獲得美味咖啡。'
         },
       ],
       benefits: [
@@ -68,12 +73,12 @@ const app = Vue.createApp({
             {
               icon: 'bi bi-person-check',
               title: '兼顧專注手沖和細微客戶服務',
-              text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+              text: '咖啡師不再被束縛在吧檯，可增加與消費者交流時間'
             },
             {
               icon: 'bi bi-currency-dollar',
               title: '減少人事訓練成本',
-              text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+              text: '專業咖啡師人力配置及教育訓練成本降低，有效降低人事成本。'
             },
           ],
         },
@@ -82,12 +87,12 @@ const app = Vue.createApp({
             {
               icon: 'bi bi-shop',
               title: '快速展店',
-              text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+              text: '人力需求降低，營業模式及咖啡沖煮手法可快速複製，縮短開店所需時間。'
             },
             {
               icon: 'bi bi-check2-circle',
               title: '咖啡品質穩定',
-              text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.'
+              text: '由咖啡機執行固定沖煮參數，杯杯都是相同品質。'
             },
           ]
         }
